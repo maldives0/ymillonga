3103fc65fe6345a3c6be6c6730a9293b1be01a53
maldives0 <maliethy@gmail.com>
2020-12-17 16:09:31 +0900

    backUrlTest

--- front/config/config.js
+++ front/config/config.js
@@ -1,7 +1,9 @@
-export const backUrl = process.env.NODE_ENV === 'production' ? "http://api.ymillonga.com" : "http://localhost:3051";
+export const backUrl = process.env.NODE_ENV === 'production' ? "http://http://15.165.249.3" : "http://localhost:3051";
+// export const backUrl = process.env.NODE_ENV === 'production' ? "http://api.ymillonga.com" : "http://localhost:3051";
 // export const backUrl = "http://localhost:3051";
 
 export const frontUrl = process.env.NODE_ENV === 'production' ? "https://ymillonga.com" : "http://localhost:3050";
-export const callbackUrl = process.env.NODE_ENV === 'production' ? "http://api.ymillonga.com/user/google/callback" : "http://localhost:3051/user/google/callback";
+export const callbackUrl = process.env.NODE_ENV === 'production' ? "http://http://15.165.249.3/user/google/callback" : "http://localhost:3051/user/google/callback";
+// export const callbackUrl = process.env.NODE_ENV === 'production' ? "http://api.ymillonga.com/user/google/callback" : "http://localhost:3051/user/google/callback";
 
 
