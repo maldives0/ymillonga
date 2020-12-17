7090cc48ac0ad2008c041384b5b9b31c93c45379
maldives0 <maliethy@gmail.com>
2020-12-17 20:04:43 +0900

    back port change 80->3051

--- back/app.js
+++ back/app.js
@@ -88,6 +88,6 @@ app.use((err, req, res, next) => {
     res.render('error');
 });
 
-app.listen(port, () => {
-    console.log(`${port}에서 대기 중`);
+app.listen(3051, () => {
+    console.log(`3051에서 대기 중`);
 });
