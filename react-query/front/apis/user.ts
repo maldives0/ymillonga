f40ba00211b647f3832c74045a1b231563cc2f3e
maliethy <maliethy@gmail.com>
2021-10-29 22:27:29 +0900

    feat: apis/user.ts 정리하기

--- react-query/front/apis/user.ts
+++ react-query/front/apis/user.ts
@@ -7,3 +7,51 @@ axios.defaults.withCredentials = true;
 export function loadMyInfoAPI() {
   return axios.get("/user").then((response) => response.data);
 }
+
+export function loadUserAPI(data: { id: number }) {
+  return axios.get(`/user/${data}`);
+}
+
+export function loginAPI(data: { email: string; password: string }) {
+  return axios.post("/user/login", data);
+}
+
+export function logoutAPI() {
+  return axios.post("/user/logout");
+}
+
+export function signupAPI(data: {
+  email: string;
+  nickname: string;
+  password: string;
+}) {
+  return axios.post("/user", data);
+}
+
+export function changeNicknameAPI(data: string) {
+  return axios.patch("/user/nickname", { nickname: data });
+}
+
+export function ignoreAPI(data: { id: number }) {
+  return axios.patch(`/user/${data}/ignore`);
+}
+
+export function unIgnoreAPI(data: { id: number }) {
+  return axios.delete(` /user/${data}/ignore`);
+}
+
+export function followAPI(data: { id: number }) {
+  return axios.patch(`/user/${data}/follow`);
+}
+
+export function unfollowAPI(data: { id: number }) {
+  return axios.delete(`/user/${data}/follow`);
+}
+
+export function removeFollowerAPI(data: { id: number }) {
+  return axios.delete(`/user/follower/${data}`);
+}
+
+export function leaveAPI() {
+  return axios.delete(`/user/leave`);
+}
